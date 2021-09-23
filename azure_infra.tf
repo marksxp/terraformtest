36d8342f2b11153dda55ed018fce7a7f6eb20f7f
marksxp <63218062+marksxp@users.noreply.github.com>
2021-09-23 16:29:54 -0500

    mi primera version

---
+++ azure_infra.tf
@@ -0,0 +1,29 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "MyResourceGroup"
+  location = "East US"
+}
+
+resource "azurerm_kubernetes_cluster" "example" {
+  name                = "mykubernetescluster1"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  dns_prefix          = "exampleaks1"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 1
+    vm_size    = "Standard_D2_v2"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  tags = {
+    Environment = "Production"
+  }
+}
